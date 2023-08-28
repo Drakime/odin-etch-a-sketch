4f74d972e0ffa02a12e4bba6a6ceaa311c7dcf5c
Drakime <42014144+Drakime@users.noreply.github.com>
2023-08-28 20:52:07 +0100

    Add feature to change grid square color on hover
    Currently, it is assumed that the size of the grid
    is 16 x 16. However, what will need to be done is to
    create the grid dynamically, later allowing the user
    to set the size of the grid manually.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 let grid = document.getElementById("grid");
 
-// Create and add 16 * 16 squares to the grid
-for (let i = 0; i < (16 * 16); i++) {
-  let gridSquare = document.createElement("div");
-  gridSquare.classList.add("gridSquare");
+function createGrid() {
+  // Create and add 16 * 16 squares to the grid
+  for (let i = 0; i < (16 * 16); i++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
 
-  grid.append(gridSquare);
+    grid.append(gridSquare);
+  }
 }
+
+function addGridSquareEventListeners() {
+  const squares = document.querySelectorAll(".gridSquare");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "red";
+    })
+  })
+}
+
+function initialiseGrid() {
+  createGrid();
+  addGridSquareEventListeners();
+}
+
+document.addEventListener('DOMContentLoaded', initialiseGrid);
